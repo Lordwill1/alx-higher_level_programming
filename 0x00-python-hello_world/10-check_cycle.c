4d008d2aa9a02ca997ad6aed0f11427332eb7e18
Lordwill1 <messagelordwill@gmail.com>
2022-11-28 20:53:34 +0000

    Python project

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for loop in LL
+ * @list: head of linked list
+ *
+ * Description - check for loops in LL
+ * Return: 1 if cycled, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (!list)
+	{
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str * 3)
+print(str[:9])

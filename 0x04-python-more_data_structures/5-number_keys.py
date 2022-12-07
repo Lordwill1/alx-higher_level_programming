4cc22cf4fe8245dc6713469a14ccbfa1f92cd9f6
Godswill Kalu <messagelordwill@gmail.com>
2022-12-08 00:33:28 +0100

    Update 5-number_keys.py
    A function that returns the number of keys in a dictionary.

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
+
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-    return (num)
+    """
+    A function that returns the number of keys in a dictionary.
+    """
+    return len(a_dictionary)

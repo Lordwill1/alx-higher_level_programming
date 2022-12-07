bdfcc70b371c6f4846bfd0fd9a5c78f3fc5d36d5
Godswill Kalu <messagelordwill@gmail.com>
2022-12-08 00:38:24 +0100

    Update 10-best_score.py
    A function that returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
 
-    return (max(a_dictionary, key=a_dictionary.get))
+
+def best_score(a_dictionary):
+    """
+    A function that returns a key with the biggest integer value.
+    """
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+        score = 0
+        leader = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                leader = i
+        return leader

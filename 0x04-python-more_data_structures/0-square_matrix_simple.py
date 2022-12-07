72a8192e9085965858da3f41a27f1f8c50cc1b6c
Lordwill1 <messagelordwill@gmail.com>
2022-12-07 23:00:13 +0000

    Updated 0-square_matrix_simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,12 +2,4 @@
 
 
 def square_matrix_simple(matrix=[]):
-    """
-    wordA function that computes the square
-    value of all integers of a matrix.
-    """
-    new_matrix = []
-    for col in matrix:
-        result = list(map(lambda x: x**2, col))
-        new_matrix.append(result)
-    return new_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])

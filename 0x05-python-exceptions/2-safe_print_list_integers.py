87f2094edc715503f55903a6b2c2b8062b7f6578
Lordwill1 <messagelordwill@gmail.com>
2022-12-20 05:59:55 +0000

    Added files for project 0x05-python

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
